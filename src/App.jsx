a7311ba3f1eb7bee142805cff0da02f66155cd0b
pranavmali-create <pranavmali0107@gmail.com>
2026-01-23 17:54:20 +0530

    feat(project): set up initial react vite app with tailwind and eslint

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+import "./App.css";
+// import Card from "./components/Card";
+// import Card2 from "./components/Card2";
+// import Card3 from "./components/Card3";
+// import Card4 from "./components/Card4";
+// import Card5 from "./components/Card5";
+import Card6 from "./components/Card6";
+function App() {
+  return (
+    <>
+      {/* <Card /> */}
+      {/* <Card2 /> */}
+      {/* <Card3 /> */}
+      {/* <Card4 /> */}
+      {/* <Card5 /> */}
+      <Card6 />
+    </>
+  );
+}
+
+export default App;
